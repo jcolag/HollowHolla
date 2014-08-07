be090b9b87bc62648282be0c2babc62e59bdc248
John Colagioia <john@colagioia.net>
2014-08-07 14:16:01 -0400

    Base MVC Project

---
+++ HollowHolla.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using HollowHolla;
+using HollowHolla.Controllers;
+
+namespace HollowHolla.Tests
+{
+        [TestFixture()]
+        public class HomeControllerTest
+        {
+                [Test()]
+                public void Index()
+                {
+                        // Arrange
+                        HomeController controller = new HomeController();
+
+                        // Act
+                        ViewResult result = controller.Index() as ViewResult;
+
+                        // Assert
+                        Assert.AreEqual("Welcome to ASP.NET MVC on Mono!", result.ViewData["Message"]);
+                }
+        }
+}
