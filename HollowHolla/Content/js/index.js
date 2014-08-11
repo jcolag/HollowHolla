23ca2a67e50d8543448a3a6de46c097a279339db
John Colagioia <john@colagioia.net>
2014-08-11 06:30:31 -0400

    Format messages

--- HollowHolla/Content/js/index.js
+++ HollowHolla/Content/js/index.js
@@ -1,13 +1,17 @@
 ﻿function UpdateMessages() {
-    var url = "/Home/Next";
+	var url = "/Home/Next";
     var inner = "";
-    $.get(url, function(data) {
-        if ((data === "") || (data === null)) {
-            return;
-        }
-        var post = "<li class='hh-post'>" + data.replace(/#/g, "<br>") + "</li>";
+	$.get(url, function(data) {
+		if ((data === "") || (data === null)) {
+			return;
+		}
+		var items = data.split('#');
+		var post = "<li class='hh-post'>";
+		post += "<h2>" + items[0] + "</h2>";
+		post += "<i>" + items[1].replace(/\*/g, '') + "</i><br>";
+		post += items[2] + "</li>";
 		$('#stream').append(post);
-    });
+	});
 }
 $(document).ready(function() {
 	setInterval(function() {
