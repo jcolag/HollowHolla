8617ab361a5b318f75aa7aee31ea5524ec408ac9
John Colagioia <john@colagioia.net>
2014-08-26 10:55:33 -0400

    Add timestamps to posts

--- HollowHolla/Content/js/index.js
+++ HollowHolla/Content/js/index.js
@@ -8,10 +8,21 @@
 		var items = data.split('#');
 		var post = "<li class='hh-post'>";
 		post += "<h2>" + items[0] + "</h2>";
+		if (items.length > 3) {
+		    post += "<i><small>" + FormatTime(items[3]) + "</small></i><br>";
+		}
 		post += "<i>" + items[1].replace(/\*/g, '') + "</i><br>";
 		post += items[2] + "</li>";
 		$('#stream').append(post);
 	});
+}
+function FormatTime(code) {
+	var year = new Date().getFullYear();
+	var month = code.substring(0, 2);
+	var day = code.substring(2, 4);
+	var hour = code.substring(4, 6);
+	var minute = code.substring(6, 8);
+	return "" + year + "/" + month + "/" + day + " " + hour + ":" + minute;
 }
 $(document).ready(function() {
 	setInterval(function() {
